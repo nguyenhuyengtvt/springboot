e3a5190672184d5c6be465226462858e9d5d753f
Nguyen Van Huyen <nguyenhuyengtvt@gmail.com>
2018-09-06 15:38:38 +0700

    sysout

--- src/main/java/com/ts24/springboot/SpringbootApplication.java
+++ src/main/java/com/ts24/springboot/SpringbootApplication.java
@@ -1,12 +1,22 @@
 package com.ts24.springboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		PasswordEncoder endcoder = new BCryptPasswordEncoder();
+		System.out.println(endcoder.encode("123456"));
+		System.out.println("max: " + Integer.MAX_VALUE);
 	}
 }
